de9a508318e8d56324d0a2a274864d89850bef07
autom8ter <admin@autom8ter.com>
2019-04-06 22:19:45 -0600

    checkpoint

--- go.mod
+++ go.mod
@@ -1,48 +1,22 @@
 module github.com/autom8ter/goconnect
 
 require (
-	cloud.google.com/go v0.37.2
-	dmitri.shuralyov.com/app/changes v0.0.0-20180602232624-0a106ad413e3 // indirect
-	dmitri.shuralyov.com/html/belt v0.0.0-20180602232347-f7d459c86be0 // indirect
-	dmitri.shuralyov.com/service/change v0.0.0-20181023043359-a85b471d5412 // indirect
-	dmitri.shuralyov.com/state v0.0.0-20180228185332-28bcc343414c // indirect
-	firebase.google.com/go v3.6.0+incompatible
-	github.com/autom8ter/engine v0.0.0-20190329053011-dc0f5aa62cf8
+	github.com/Masterminds/sprig v2.18.0+incompatible
+	github.com/autom8ter/commander v0.0.0-20190403035737-5d09bd7cace9 // indirect
 	github.com/autom8ter/gcloud v0.0.0-20190405045739-7cb869700bc9
-	github.com/autom8ter/gocrypt v0.0.0-20190405014227-981cb38a93ea
-	github.com/autom8ter/gosaas v0.0.0-20190403043001-2edc013f8640 // indirect
-	github.com/autom8ter/gosub v0.0.0-20190404052952-4275f750c402
-	github.com/microcosm-cc/bluemonday v1.0.1 // indirect
-	github.com/neelance/astrewrite v0.0.0-20160511093645-99348263ae86 // indirect
-	github.com/neelance/sourcemap v0.0.0-20151028013722-8c68805598ab // indirect
+	github.com/autom8ter/gosaas v0.0.0-20190407022111-b15e0670f7dc
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fatih/structs v1.1.0
+	github.com/golang/protobuf v1.3.1
+	github.com/google/uuid v1.1.1
 	github.com/nlopes/slack v0.5.0
 	github.com/pkg/errors v0.8.1
+	github.com/sendgrid/rest v2.4.1+incompatible // indirect
 	github.com/sendgrid/sendgrid-go v3.4.1+incompatible
 	github.com/sfreiberg/gotwilio v0.0.0-20181223013140-ccf5c3cb3e06
-	github.com/shurcooL/component v0.0.0-20170202220835-f88ec8f54cc4 // indirect
-	github.com/shurcooL/events v0.0.0-20181021180414-410e4ca65f48 // indirect
-	github.com/shurcooL/github_flavored_markdown v0.0.0-20181002035957-2122de532470 // indirect
-	github.com/shurcooL/gofontwoff v0.0.0-20180329035133-29b52fc0a18d // indirect
-	github.com/shurcooL/gopherjslib v0.0.0-20160914041154-feb6d3990c2c // indirect
-	github.com/shurcooL/highlight_diff v0.0.0-20170515013008-09bb4053de1b // indirect
-	github.com/shurcooL/highlight_go v0.0.0-20181028180052-98c3abbbae20 // indirect
-	github.com/shurcooL/home v0.0.0-20181020052607-80b7ffcb30f9 // indirect
-	github.com/shurcooL/htmlg v0.0.0-20170918183704-d01228ac9e50 // indirect
-	github.com/shurcooL/httperror v0.0.0-20170206035902-86b7830d14cc // indirect
-	github.com/shurcooL/httpfs v0.0.0-20171119174359-809beceb2371 // indirect
-	github.com/shurcooL/httpgzip v0.0.0-20180522190206-b1c53ac65af9 // indirect
-	github.com/shurcooL/issues v0.0.0-20181008053335-6292fdc1e191 // indirect
-	github.com/shurcooL/issuesapp v0.0.0-20180602232740-048589ce2241 // indirect
-	github.com/shurcooL/notifications v0.0.0-20181007000457-627ab5aea122 // indirect
-	github.com/shurcooL/octicon v0.0.0-20181028054416-fa4f57f9efb2 // indirect
-	github.com/shurcooL/reactions v0.0.0-20181006231557-f2e0b4ca5b82 // indirect
-	github.com/shurcooL/sanitized_anchor_name v0.0.0-20170918181015-86672fcb3f95 // indirect
-	github.com/shurcooL/users v0.0.0-20180125191416-49c67e49c537 // indirect
-	github.com/shurcooL/webdavfs v0.0.0-20170829043945-18c3829fa133 // indirect
-	github.com/sourcegraph/annotate v0.0.0-20160123013949-f4cad6c6324d // indirect
-	github.com/sourcegraph/syntaxhighlight v0.0.0-20170531221838-bd320f5d308e // indirect
 	github.com/stripe/stripe-go v60.0.0+incompatible
-	gocloud.dev v0.12.0 // indirect
+	golang.org/x/crypto v0.0.0-20190325154230-a5d413f7728c
 	google.golang.org/api v0.3.0
-	sourcegraph.com/sourcegraph/go-diff v0.5.0 // indirect
+	gopkg.in/go-playground/validator.v9 v9.28.0
+	gopkg.in/yaml.v2 v2.2.2
 )
--- goconnect.go
+++ goconnect.go
@@ -3,7 +3,8 @@ package goconnect
 import (
 	"context"
 	"github.com/autom8ter/gcloud"
-	"github.com/autom8ter/gocrypt"
+	"github.com/autom8ter/goconnect/util"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/nlopes/slack"
 	"github.com/pkg/errors"
 	"github.com/sendgrid/sendgrid-go"
@@ -11,21 +12,23 @@ import (
 	"github.com/stripe/stripe-go"
 	"github.com/stripe/stripe-go/client"
 	"google.golang.org/api/option"
-	"log"
+	"io"
 	"net/http"
 )
 
 //Config holds the required configuration variables to create a GoConnect Instance
 type Config struct {
-	GCPCredsPath    string `json:"firebase_creds_path"`
-	TwilioAccount   string `json:"twilio_account"`
-	TwilioToken     string `json:"twilio_token"`
-	SendGridAccount string `json:"sendgrid_account"`
-	SendGridToken   string `json:"sendgrid_token"`
-	StripeAccount   string `json:"stripe_account"`
-	StripeToken     string `json:"stripe_token"`
-	SlackAccount    string `json:"slack_account"`
-	SlackToken      string `json:"slack_token"`
+	GCPCredsPath    string   `json:"firebase_creds_path"`
+	TwilioAccount   string   `json:"twilio_account"`
+	TwilioToken     string   `json:"twilio_token"`
+	SendGridAccount string   `json:"sendgrid_account"`
+	SendGridToken   string   `json:"sendgrid_token"`
+	StripeAccount   string   `json:"stripe_account"`
+	StripeToken     string   `json:"stripe_token"`
+	SlackAccount    string   `json:"slack_account"`
+	SlackToken      string   `json:"slack_token"`
+	Scopes          []string `json:"scopes"`
+	MasterKey       string   `json:"master_key"`
 }
 
 // GoConnect holds an authenticated Twilio, Stripe, Firebase, and SendGrid Client. It also carries an HTTP client and context.
@@ -37,31 +40,40 @@ type GoConnect struct {
 	strip *client.API
 	chat  *slack.Client
 	gcp   *gcloud.GCP
-	crypt *gocrypt.GoCrypt
 	cli   *http.Client
+	data  map[string]interface{}
+}
+
+type MyCustomClaims struct {
+	Account string `json:"account"`
+	Email   string `json:"email"`
+	Phone   string `json:"phone"`
+	jwt.StandardClaims
 }
 
 // New Creates a new GoConnect from the provided http client and config
-func New(cli *http.Client, c *Config) *GoConnect {
-	ctx := context.Background()
-	if cli == nil {
-		cli = http.DefaultClient
-	}
-
-	gcp, err := gcloud.New(ctx, option.WithCredentialsFile(c.GCPCredsPath))
+func New(ctx context.Context, c *Config) (*GoConnect, error) {
+	g := &GoConnect{}
+	var err error
+	g.ctx = ctx
+	g.cfg = c
+	g.gcp, err = gcloud.New(ctx, option.WithCredentialsFile(c.GCPCredsPath))
 	if err != nil {
-		log.Fatalln(err.Error())
+		err = util.WrapErr(err, "failed to create gcloud client")
 	}
-	return &GoConnect{
-		cfg:   c,
-		twil:  gotwilio.NewTwilioClientCustomHTTP(c.TwilioAccount, c.TwilioToken, cli),
-		grid:  sendgrid.NewSendClient(c.SendGridToken),
-		strip: client.New(c.StripeToken, stripe.NewBackends(cli)),
-		ctx:   ctx,
-		chat:  slack.New(c.SlackToken),
-		crypt: gocrypt.NewGoCrypt(),
-		gcp:   gcp,
+	g.cli, err = g.gcp.DefaultClient(ctx, c.Scopes)
+	if err != nil {
+		err = util.WrapErr(err, "failed to create http client from config scopes")
 	}
+	g.chat = slack.New(c.SlackToken)
+	g.twil = gotwilio.NewTwilioClientCustomHTTP(c.TwilioAccount, c.TwilioToken, g.cli)
+	g.strip = client.New(c.StripeToken, stripe.NewBackends(g.cli))
+	g.grid = sendgrid.NewSendClient(c.SendGridToken)
+	g.data = make(map[string]interface{})
+	if err != nil {
+		return g, err
+	}
+	return g, nil
 }
 
 // GCP returns a gcloud.GCP instance
@@ -94,18 +106,113 @@ func (g *GoConnect) Slack() *slack.Client {
 	return g.chat
 }
 
-// Twilio returns an HTTP client
+// HTTP returns an HTTP client
 func (g *GoConnect) HTTP() *http.Client {
 	return g.cli
 }
 
-// GoSub returns a GoCrypt client
-func (g *GoConnect) GoCrypt() *gocrypt.GoCrypt {
-	return g.crypt
+// Render renders the text with the GoConnects current data. It writes the output to the provided writer
+func (g *GoConnect) Render(text string, w io.Writer) error {
+	return util.Render(text, g.data, w)
 }
 
+// JSON returns the GoConnects current data as JSON
+func (g *GoConnect) JSON() []byte {
+	return util.JSON(g.data)
+}
+
+// YAML returns the GoConnects current data as YAML
+func (g *GoConnect) YAML() []byte {
+	return util.YAML(g.data)
+}
+
+// XML returns the GoConnects current data as XML
+func (g *GoConnect) XML() []byte {
+	return util.XML(g.data)
+}
+
+// Validate validates the provided object and returns an error if invalid ref: https://github.com/go-playground/validator
+func (g *GoConnect) Validate(obj interface{}) error {
+	return util.Validate(obj)
+}
+
+// Data returns GoConnects current data as map[string]interface{}
+func (g *GoConnect) Data() map[string]interface{} {
+	return g.data
+}
+
+// AddStructData appends the provided structs data to the GoConnects data
+func (g *GoConnect) AddStructData(obj interface{}) {
+	for k, v := range util.AsMap(obj) {
+		g.data[k] = v
+	}
+}
+
+// AddData appends the provided data to the GoConnects current data
+func (g *GoConnect) AddData(obj map[string]interface{}) {
+	for k, v := range obj {
+		g.data[k] = v
+	}
+}
+
+// MasterKey returns the master key from config as bytes. Defaults to "secret"
+func (g *GoConnect) MasterKey() []byte {
+	if g.cfg.MasterKey != "" {
+		return []byte(g.cfg.MasterKey)
+	}
+	return []byte("secret")
+}
+
+// HashPassword uses bcrypt to hash a password string
+func (g *GoConnect) HashPassword(pass string) (string, error) {
+	return util.HashPassword(pass)
+}
+
+// CompareHashToPassWord uses bcrypt to compare the provided hash to the provided password
+func (g *GoConnect) CompareHashToPassword(hash, pass string) error {
+	return util.ComparePasswordToHash(hash, pass)
+}
+
+// WrapErr wraps the provided error with the provided message
+func (g *GoConnect) WrapErr(err error, msg string) error {
+	return util.WrapErr(err, msg)
+}
+
+// Provides UUID string. UUIDs are based on RFC 4122 and DCE 1.1: Authentication and Security Services.
+func (g *GoConnect) UUID() string {
+	return util.UUID()
+}
+
+// MustGetEnv returns the environmental variable found in the provided key. If no value is found, the provided default value is returned
+func (g *GoConnect) MustGetEnv(key string, defval string) string {
+	return util.MustGetEnv(key, defval)
+}
+
+// NewToken create a new JWT token from the provided claims
+func (g *GoConnect) NewToken(claims *MyCustomClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	/* Sign the token with our secret */
+	return token.SignedString(g.MasterKey())
+}
+
+// ValidateToken will validate the token
+func (g *GoConnect) ValidateToken(myToken string) (bool, string) {
+	token, err := jwt.ParseWithClaims(myToken, &MyCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(g.MasterKey()), nil
+	})
+
+	if err != nil {
+		return false, ""
+	}
+
+	claims := token.Claims.(*MyCustomClaims)
+	return token.Valid, claims.Email
+}
+
+// A Func is a GoConnect Callback function
 type Func func(g *GoConnect) error
 
+// Execute runs the provided functions.
 func (g *GoConnect) Execute(fns ...Func) error {
 	var err error
 	for _, f := range fns {
