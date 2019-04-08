1dfc60dca0d52d1b900cc6f2052a3fbc649362b9
autom8ter <admin@autom8ter.com>
2019-04-08 10:11:14 -0600

    stripe

--- pkg/stripe/stripe.go
+++ pkg/stripe/stripe.go
@@ -15,83 +15,175 @@ func New(client *client.API) *Stripe {
 	}
 }
 
-func (s *Stripe) GetBalance(params *stripe.BalanceParams) (*stripe.Balance, error) {
-	return s.client.Balance.Get(params)
+type BalanceOption func(params *stripe.BalanceParams)
+
+func (s *Stripe) GetBalance(opts ...BalanceOption) (*stripe.Balance, error) {
+	var p = &stripe.BalanceParams{}
+	for _, o := range opts {
+		o(p)
+	}
+	return s.client.Balance.Get(p)
 }
 
-func (s *Stripe) GetPlan(id string, params *stripe.PlanParams) (*stripe.Plan, error) {
-	return s.client.Plans.Get(id, params)
+type PlanOption func(params *stripe.PlanParams)
+
+func (s *Stripe) GetPlan(id string, opts ...PlanOption) (*stripe.Plan, error) {
+	var p = &stripe.PlanParams{}
+	for _, o := range opts {
+		o(p)
+	}
+	return s.client.Plans.Get(id, p)
 }
 
-func (s *Stripe) UpdatePlan(id string, params *stripe.PlanParams) (*stripe.Plan, error) {
+func (s *Stripe) UpdatePlan(id string, opts ...PlanOption) (*stripe.Plan, error) {
+	var params = &stripe.PlanParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Plans.Update(id, params)
 }
 
-func (s *Stripe) DeletePlan(id string, params *stripe.PlanParams) (*stripe.Plan, error) {
+func (s *Stripe) DeletePlan(id string, opts ...PlanOption) (*stripe.Plan, error) {
+	var params = &stripe.PlanParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Plans.Del(id, params)
 }
 
-func (s *Stripe) NewPlan(params *stripe.PlanParams) (*stripe.Plan, error) {
+func (s *Stripe) NewPlan(id string, opts ...PlanOption) (*stripe.Plan, error) {
+	var params = &stripe.PlanParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Plans.New(params)
 }
 
-func (s *Stripe) GetCustomer(id string, params *stripe.CustomerParams) (*stripe.Customer, error) {
+type CustomerOption func(params *stripe.CustomerParams)
+
+func (s *Stripe) GetCustomer(id string, opts ...CustomerOption) (*stripe.Customer, error) {
+	var params = &stripe.CustomerParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Customers.Get(id, params)
 }
 
-func (s *Stripe) UpdateCustomer(id string, params *stripe.CustomerParams) (*stripe.Customer, error) {
+func (s *Stripe) UpdateCustomer(id string, opts ...CustomerOption) (*stripe.Customer, error) {
+	var params = &stripe.CustomerParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Customers.Update(id, params)
 }
 
-func (s *Stripe) DeleteCustomer(id string, params *stripe.CustomerParams) (*stripe.Customer, error) {
+func (s *Stripe) DeleteCustomer(id string, opts ...CustomerOption) (*stripe.Customer, error) {
+	var params = &stripe.CustomerParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Customers.Del(id, params)
 }
 
-func (s *Stripe) NewCustomer(params *stripe.CustomerParams) (*stripe.Customer, error) {
+func (s *Stripe) NewCustomer(id string, opts ...CustomerOption) (*stripe.Customer, error) {
+	var params = &stripe.CustomerParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Customers.New(params)
 }
 
-func (s *Stripe) GetCharge(id string, params *stripe.ChargeParams) (*stripe.Charge, error) {
+type ChargeOption func(params *stripe.ChargeParams)
+
+func (s *Stripe) GetCharge(id string, opts ...ChargeOption) (*stripe.Charge, error) {
+	var params = &stripe.ChargeParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Charges.Get(id, params)
 }
 
-func (s *Stripe) NewCharge(id string, params *stripe.ChargeParams) (*stripe.Charge, error) {
+func (s *Stripe) NewCharge(id string, opts ...ChargeOption) (*stripe.Charge, error) {
+	var params = &stripe.ChargeParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Charges.New(params)
 }
 
-func (s *Stripe) UpdateCharge(id string, params *stripe.ChargeParams) (*stripe.Charge, error) {
+func (s *Stripe) UpdateCharge(id string, opts ...ChargeOption) (*stripe.Charge, error) {
+	var params = &stripe.ChargeParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Charges.Update(id, params)
 }
 
-func (s *Stripe) GetSubscription(id string, params *stripe.SubscriptionParams) (*stripe.Subscription, error) {
+type SubscriptionOption func(params *stripe.SubscriptionParams)
+
+func (s *Stripe) GetSubscription(id string, opts ...SubscriptionOption) (*stripe.Subscription, error) {
+	var params = &stripe.SubscriptionParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Subscriptions.Get(id, params)
 }
 
-func (s *Stripe) NewSubscription(params *stripe.SubscriptionParams) (*stripe.Subscription, error) {
+func (s *Stripe) NewSubscription(opts ...SubscriptionOption) (*stripe.Subscription, error) {
+	var params = &stripe.SubscriptionParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Subscriptions.New(params)
 }
 
-func (s *Stripe) UpdateSubscription(id string, params *stripe.SubscriptionParams) (*stripe.Subscription, error) {
+func (s *Stripe) UpdateSubscription(id string, opts ...SubscriptionOption) (*stripe.Subscription, error) {
+	var params = &stripe.SubscriptionParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Subscriptions.Update(id, params)
 }
 
-func (s *Stripe) DeleteSubscription(id string, params *stripe.SubscriptionParams) (*stripe.Subscription, error) {
+func (s *Stripe) DeleteSubscription(id string, opts ...SubscriptionOption) (*stripe.Subscription, error) {
+	var params = &stripe.SubscriptionParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Subscriptions.Update(id, params)
 }
 
-func (s *Stripe) GetInvoice(id string, params *stripe.InvoiceParams) (*stripe.Invoice, error) {
+type InvoiceOption func(params *stripe.InvoiceParams)
+
+func (s *Stripe) GetInvoice(id string, opts ...InvoiceOption) (*stripe.Invoice, error) {
+	var params = &stripe.InvoiceParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Invoices.Get(id, params)
 }
 
-func (s *Stripe) NewInvoice(params *stripe.InvoiceParams) (*stripe.Invoice, error) {
+func (s *Stripe) NewInvoice(opts ...InvoiceOption) (*stripe.Invoice, error) {
+	var params = &stripe.InvoiceParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Invoices.New(params)
 }
 
-func (s *Stripe) UpdateInvoice(id string, params *stripe.InvoiceParams) (*stripe.Invoice, error) {
+func (s *Stripe) UpdateInvoice(id string, opts ...InvoiceOption) (*stripe.Invoice, error) {
+	var params = &stripe.InvoiceParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Invoices.Update(id, params)
 }
 
-func (s *Stripe) DeleteInvoice(id string, params *stripe.InvoiceParams) (*stripe.Invoice, error) {
+func (s *Stripe) DeleteInvoice(id string, opts ...InvoiceOption) (*stripe.Invoice, error) {
+	var params = &stripe.InvoiceParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Invoices.Del(id, params)
 }
 
@@ -99,74 +191,162 @@ func (s *Stripe) GetAccount() (*stripe.Account, error) {
 	return s.client.Account.Get()
 }
 
-func (s *Stripe) GetBankAccount(id string, params *stripe.BankAccountParams) (*stripe.BankAccount, error) {
+type BankAccountOption func(params *stripe.BankAccountParams)
+
+func (s *Stripe) GetBankAccount(id string, opts ...BankAccountOption) (*stripe.BankAccount, error) {
+	var params = &stripe.BankAccountParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.BankAccounts.Get(id, params)
 }
 
-func (s *Stripe) NewBankAccount(params *stripe.BankAccountParams) (*stripe.BankAccount, error) {
+func (s *Stripe) NewBankAccount(opts ...BankAccountOption) (*stripe.BankAccount, error) {
+	var params = &stripe.BankAccountParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.BankAccounts.New(params)
 }
 
-func (s *Stripe) UpdateBankAccount(id string, params *stripe.BankAccountParams) (*stripe.BankAccount, error) {
+func (s *Stripe) UpdateBankAccount(id string, opts ...BankAccountOption) (*stripe.BankAccount, error) {
+	var params = &stripe.BankAccountParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.BankAccounts.Update(id, params)
 }
 
-func (s *Stripe) DeleteBankAccount(id string, params *stripe.BankAccountParams) (*stripe.BankAccount, error) {
+func (s *Stripe) DeleteBankAccount(id string, opts ...BankAccountOption) (*stripe.BankAccount, error) {
+	var params = &stripe.BankAccountParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.BankAccounts.Del(id, params)
 }
 
-func (s *Stripe) GetProduct(id string, params *stripe.ProductParams) (*stripe.Product, error) {
+type ProductOption func(params *stripe.ProductParams)
+
+func (s *Stripe) GetProduct(id string, opts ...ProductOption) (*stripe.Product, error) {
+	var params = &stripe.ProductParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Products.Get(id, params)
 }
 
-func (s *Stripe) UpdateProduct(id string, params *stripe.ProductParams) (*stripe.Product, error) {
+func (s *Stripe) UpdateProduct(id string, opts ...ProductOption) (*stripe.Product, error) {
+	var params = &stripe.ProductParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Products.Update(id, params)
 }
 
-func (s *Stripe) DeleteProduct(id string, params *stripe.ProductParams) (*stripe.Product, error) {
+func (s *Stripe) DeleteProduct(id string, opts ...ProductOption) (*stripe.Product, error) {
+	var params = &stripe.ProductParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Products.Del(id, params)
 }
 
-func (s *Stripe) NewProduct(params *stripe.ProductParams) (*stripe.Product, error) {
+func (s *Stripe) NewProduct(opts ...ProductOption) (*stripe.Product, error) {
+	var params = &stripe.ProductParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Products.New(params)
 }
 
-func (s *Stripe) Recipients(id string, params *stripe.RecipientParams) (*stripe.Recipient, error) {
+type ReciptientOption func(params *stripe.RecipientParams)
+
+func (s *Stripe) GetRecipient(id string, opts ...ReciptientOption) (*stripe.Recipient, error) {
+	var params = &stripe.RecipientParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Recipients.Get(id, params)
 }
 
-func (s *Stripe) UpdateRecipients(id string, params *stripe.RecipientParams) (*stripe.Recipient, error) {
+func (s *Stripe) UpdateRecipients(id string, opts ...ReciptientOption) (*stripe.Recipient, error) {
+	var params = &stripe.RecipientParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Recipients.Update(id, params)
 }
 
-func (s *Stripe) DeleteRecipients(id string, params *stripe.RecipientParams) (*stripe.Recipient, error) {
+func (s *Stripe) DeleteRecipients(id string, opts ...ReciptientOption) (*stripe.Recipient, error) {
+	var params = &stripe.RecipientParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Recipients.Del(id, params)
 }
 
-func (s *Stripe) GetOrder(id string, params *stripe.OrderParams) (*stripe.Order, error) {
+type OrderOption func(params *stripe.OrderParams)
+
+func (s *Stripe) GetOrder(id string, opts ...OrderOption) (*stripe.Order, error) {
+	var params = &stripe.OrderParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Orders.Get(id, params)
 }
 
-func (s *Stripe) UpdateOrder(id string, params *stripe.OrderUpdateParams) (*stripe.Order, error) {
+type OrderUpdateOption func(params *stripe.OrderUpdateParams)
+
+func (s *Stripe) UpdateOrder(id string, opts ...OrderUpdateOption) (*stripe.Order, error) {
+	var params = &stripe.OrderUpdateParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Orders.Update(id, params)
 }
 
-func (s *Stripe) PayOrder(id string, params *stripe.OrderPayParams) (*stripe.Order, error) {
+type OrderPayOption func(params *stripe.OrderPayParams)
+
+func (s *Stripe) PayOrder(id string, opts ...OrderPayOption) (*stripe.Order, error) {
+	var params = &stripe.OrderPayParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Orders.Pay(id, params)
 }
 
-func (s *Stripe) ReturnOrder(id string, params *stripe.OrderReturnParams) (*stripe.OrderReturn, error) {
+type OrderReturnOption func(params *stripe.OrderReturnParams)
+
+func (s *Stripe) ReturnOrder(id string, opts ...OrderReturnOption) (*stripe.OrderReturn, error) {
+	var params = &stripe.OrderReturnParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Orders.Return(id, params)
 }
 
-func (s *Stripe) GetPayout(id string, params *stripe.PayoutParams) (*stripe.Payout, error) {
+type PayoutOption func(params *stripe.PayoutParams)
+
+func (s *Stripe) GetPayout(id string, opts ...PayoutOption) (*stripe.Payout, error) {
+	var params = &stripe.PayoutParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Payouts.Get(id, params)
 }
 
-func (s *Stripe) NewPayout(params *stripe.PayoutParams) (*stripe.Payout, error) {
+func (s *Stripe) NewPayout(opts ...PayoutOption) (*stripe.Payout, error) {
+	var params = &stripe.PayoutParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Payouts.New(params)
 }
 
-func (s *Stripe) UpdatePayout(id string, params *stripe.PayoutParams) (*stripe.Payout, error) {
+func (s *Stripe) UpdatePayout(id string, opts ...PayoutOption) (*stripe.Payout, error) {
+	var params = &stripe.PayoutParams{}
+	for _, o := range opts {
+		o(params)
+	}
 	return s.client.Payouts.Update(id, params)
 }
