0c2052542aff7da2d38c32931f43c74b7475ba63
autom8ter <admin@autom8ter.com>
2019-04-08 09:43:09 -0600

    goconnect

--- pkg/stripe/stripe.go
+++ pkg/stripe/stripe.go
@@ -31,12 +31,44 @@ func (s *Stripe) GetCharge(id string, params *stripe.ChargeParams) (*stripe.Char
 	return s.client.Charges.Get(id, params)
 }
 
+func (s *Stripe) NewCharge(id string, params *stripe.ChargeParams) (*stripe.Charge, error) {
+	return s.client.Charges.New(params)
+}
+
+func (s *Stripe) UpdateCharge(id string, params *stripe.ChargeParams) (*stripe.Charge, error) {
+	return s.client.Charges.Update(id, params)
+}
+
 func (s *Stripe) GetSubscription(id string, params *stripe.SubscriptionParams) (*stripe.Subscription, error) {
 	return s.client.Subscriptions.Get(id, params)
 }
 
+func (s *Stripe) NewSubscription(params *stripe.SubscriptionParams) (*stripe.Subscription, error) {
+	return s.client.Subscriptions.New(params)
+}
+
+func (s *Stripe) UpdateSubscription(id string, params *stripe.SubscriptionParams) (*stripe.Subscription, error) {
+	return s.client.Subscriptions.Update(id, params)
+}
+
+func (s *Stripe) DeleteSubscription(id string, params *stripe.SubscriptionParams) (*stripe.Subscription, error) {
+	return s.client.Subscriptions.Update(id, params)
+}
+
 func (s *Stripe) GetInvoice(id string, params *stripe.InvoiceParams) (*stripe.Invoice, error) {
 	return s.client.Invoices.Get(id, params)
+}
+
+func (s *Stripe) NewInvoice(params *stripe.InvoiceParams) (*stripe.Invoice, error) {
+	return s.client.Invoices.New(params)
+}
+
+func (s *Stripe) UpdateInvoice(id string, params *stripe.InvoiceParams) (*stripe.Invoice, error) {
+	return s.client.Invoices.Update(id, params)
+}
+
+func (s *Stripe) DeleteInvoice(id string, params *stripe.InvoiceParams) (*stripe.Invoice, error) {
+	return s.client.Invoices.Del(id, params)
 }
 
 func (s *Stripe) GetAccount() (*stripe.Account, error) {
@@ -47,18 +79,70 @@ func (s *Stripe) GetBankAccount(id string, params *stripe.BankAccountParams) (*s
 	return s.client.BankAccounts.Get(id, params)
 }
 
+func (s *Stripe) NewBankAccount(params *stripe.BankAccountParams) (*stripe.BankAccount, error) {
+	return s.client.BankAccounts.New(params)
+}
+
+func (s *Stripe) UpdateBankAccount(id string, params *stripe.BankAccountParams) (*stripe.BankAccount, error) {
+	return s.client.BankAccounts.Update(id, params)
+}
+
+func (s *Stripe) DeleteBankAccount(id string, params *stripe.BankAccountParams) (*stripe.BankAccount, error) {
+	return s.client.BankAccounts.Del(id, params)
+}
+
 func (s *Stripe) GetProduct(id string, params *stripe.ProductParams) (*stripe.Product, error) {
 	return s.client.Products.Get(id, params)
+}
+
+func (s *Stripe) UpdateProduct(id string, params *stripe.ProductParams) (*stripe.Product, error) {
+	return s.client.Products.Update(id, params)
+}
+
+func (s *Stripe) DeleteProduct(id string, params *stripe.ProductParams) (*stripe.Product, error) {
+	return s.client.Products.Del(id, params)
+}
+
+func (s *Stripe) NewProduct(params *stripe.ProductParams) (*stripe.Product, error) {
+	return s.client.Products.New(params)
 }
 
 func (s *Stripe) Recipients(id string, params *stripe.RecipientParams) (*stripe.Recipient, error) {
 	return s.client.Recipients.Get(id, params)
 }
 
+func (s *Stripe) UpdateRecipients(id string, params *stripe.RecipientParams) (*stripe.Recipient, error) {
+	return s.client.Recipients.Update(id, params)
+}
+
+func (s *Stripe) DeleteRecipients(id string, params *stripe.RecipientParams) (*stripe.Recipient, error) {
+	return s.client.Recipients.Del(id, params)
+}
+
 func (s *Stripe) GetOrder(id string, params *stripe.OrderParams) (*stripe.Order, error) {
 	return s.client.Orders.Get(id, params)
 }
 
+func (s *Stripe) UpdateOrder(id string, params *stripe.OrderUpdateParams) (*stripe.Order, error) {
+	return s.client.Orders.Update(id, params)
+}
+
+func (s *Stripe) PayOrder(id string, params *stripe.OrderPayParams) (*stripe.Order, error) {
+	return s.client.Orders.Pay(id, params)
+}
+
+func (s *Stripe) ReturnOrder(id string, params *stripe.OrderReturnParams) (*stripe.OrderReturn, error) {
+	return s.client.Orders.Return(id, params)
+}
+
 func (s *Stripe) GetPayout(id string, params *stripe.PayoutParams) (*stripe.Payout, error) {
 	return s.client.Payouts.Get(id, params)
+}
+
+func (s *Stripe) NewPayout(params *stripe.PayoutParams) (*stripe.Payout, error) {
+	return s.client.Payouts.New(params)
+}
+
+func (s *Stripe) UpdatePayout(id string, params *stripe.PayoutParams) (*stripe.Payout, error) {
+	return s.client.Payouts.Update(id, params)
 }
