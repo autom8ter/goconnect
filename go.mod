68ac09dfc2bed04df0820fd0d67ca20d48be77d0
autom8ter <admin@autom8ter.com>
2019-04-09 21:28:56 -0600

    checkpoint

--- go.mod
+++ go.mod
@@ -1,34 +1,17 @@
 module github.com/autom8ter/goconnect
 
 require (
-	cloud.google.com/go v0.37.2
-	github.com/autom8ter/api v0.0.0-20190401234042-1a81fe273705 // indirect
-	github.com/autom8ter/commander v0.0.0-20190403035737-5d09bd7cace9 // indirect
-	github.com/autom8ter/engine v0.0.0-20190408072237-fc31dfcf0858
-	github.com/autom8ter/gcloud v0.0.0-20190408073117-8b7a9496ac0d
-	github.com/autom8ter/goflix v0.0.0-20190405032258-57789c77b011 // indirect
-	github.com/autom8ter/gosaas v0.0.0-20190408094412-0183b467159f
-	github.com/autom8ter/gosdk v0.0.0-20190401043200-737af3c84563 // indirect
-	github.com/autom8ter/objectify v0.0.0-20190408071719-79ea764cf5ba
-	github.com/coreos/etcd v3.3.11+incompatible // indirect
-	github.com/fatih/structs v1.1.0
-	github.com/gemnasium/migrate v1.4.1 // indirect
-	github.com/go-pg/pg v8.0.3+incompatible // indirect
-	github.com/googlemaps/google-maps-services-go v0.0.0-20190311183511-743053230cec // indirect
-	github.com/h2non/bimg v1.0.19 // indirect
-	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
-	github.com/lileio/image_service v0.0.0-20170420184514-a2990eef1d74 // indirect
-	github.com/pkg/errors v0.8.1
+	github.com/autom8ter/objectify v0.0.0-20190410032617-585a176ff76d
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/sendgrid/rest v2.4.1+incompatible // indirect
 	github.com/sendgrid/sendgrid-go v3.4.1+incompatible
 	github.com/sfreiberg/gotwilio v0.0.0-20181223013140-ccf5c3cb3e06
+	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/stripe/stripe-go v60.0.0+incompatible
-	golang.org/x/crypto v0.0.0-20190404164418-38d8ce5564a5
-	golang.org/x/text v0.3.1-0.20180807135948-17ff2d5776d2
-	google.golang.org/api v0.3.0
-	google.golang.org/grpc v1.19.1
-	googlemaps.github.io/maps v0.0.0-20190311183511-743053230cec // indirect
-	googlemaps.github.io/maps/internal v0.0.0-20190311183511-743053230cec // indirect
-	gopkg.in/go-playground/validator.v9 v9.28.0
-	mellium.im/sasl v0.2.1 // indirect
+	golang.org/x/net v0.0.0-20190324223953-e3b2ff56ed87 // indirect
+	golang.org/x/text v0.3.1-0.20180807135948-17ff2d5776d2 // indirect
+	google.golang.org/genproto v0.0.0-20190401181712-f467c93bbac2 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- goconnect.go
+++ goconnect.go
@@ -1,35 +1,31 @@
 package goconnect
 
 import (
-	"github.com/autom8ter/gcloud"
 	"github.com/autom8ter/objectify"
 	"github.com/sendgrid/sendgrid-go"
 	"github.com/sfreiberg/gotwilio"
 	"github.com/stripe/stripe-go"
 	cli "github.com/stripe/stripe-go/client"
-	"google.golang.org/api/option"
 	"net/http"
 	"os"
 )
 
-var tool = objectify.New()
-
 //	GoConnect holds the required configuration variables to create a GoConnect Instance. Use Init() to validate a GoConnect instance.
 type GoConnect struct {
-	GCP             *gcloud.GCP `validate:"required"`
-	TwilioAccount   string      `validate:"required"`
-	TwilioToken     string      `validate:"required"`
+	TwilioAccount   string `validate:"required"`
+	TwilioToken     string `validate:"required"`
 	SendGridAccount string
 	SendGridToken   string `validate:"required"`
 	StripeAccount   string
 	StripeToken     string `validate:"required"`
 	SlackAccount    string
-	SlackToken      string `validate:"required"`
+	SlackToken      string             `validate:"required"`
+	util            *objectify.Handler `validate:"required"`
 }
 
 // New creates a new GoConnect Instance (no magic)
-func New(twilioAccount string, twilioToken string, sendGridToken string, stripeAccount string, stripeToken string, slackToken string, opts ...option.ClientOption) *GoConnect {
-	return &GoConnect{GCP: gcloud.NewGCP(opts...), TwilioAccount: twilioAccount, TwilioToken: twilioToken, SendGridToken: sendGridToken, StripeAccount: stripeAccount, StripeToken: stripeToken, SlackToken: slackToken}
+func New(twilioAccount string, twilioToken string, sendGridToken string, stripeAccount string, stripeToken string, slackToken string) *GoConnect {
+	return &GoConnect{TwilioAccount: twilioAccount, TwilioToken: twilioToken, SendGridToken: sendGridToken, StripeAccount: stripeAccount, StripeToken: stripeToken, SlackToken: slackToken}
 }
 
 // NewFromFileEnv Initializes a gcp instance from service account credentials ref: https://cloud.google.com/iam/docs/creating-managing-service-accounts#iam-service-accounts-create-console
@@ -37,7 +33,6 @@ func New(twilioAccount string, twilioToken string, sendGridToken string, stripeA
 // vars: TWILIO_ACCOUNT, TWILIO_ACCOUNT, SENDGRID_ACCOUNT, SENDGRID_TOKEN, STRIPE_ACCOUNT, STRIPE_TOKEN, SLACK_ACCOUNT, SLACK_TOKEN
 func NewFromFileEnv(file string) *GoConnect {
 	return &GoConnect{
-		GCP:             gcloud.NewGCP(option.WithCredentialsFile(file)),
 		TwilioAccount:   os.Getenv("TWILIO_ACCOUNT"),
 		TwilioToken:     os.Getenv("TWILIO_ACCOUNT"),
 		SendGridAccount: os.Getenv("SENDGRID_ACCOUNT"),
@@ -46,41 +41,34 @@ func NewFromFileEnv(file string) *GoConnect {
 		StripeToken:     os.Getenv("STRIPE_TOKEN"),
 		SlackAccount:    os.Getenv("SLACK_ACCOUNT"),
 		SlackToken:      os.Getenv("SLACK_TOKEN"),
+		util:            objectify.Default(),
 	}
 }
 
 // Init returns an error if any of the required fields are nil
 func (g *GoConnect) Init() error {
-	tool.PanicIfNil(g)
-	return tool.Validate(g)
-}
-
-// ToMap returns the GoConnect config as a map
-func (g *GoConnect) ToMap() map[string]interface{} {
-	tool.PanicIfNil(g)
-	return tool.ToMap(g)
+	return g.util.Validate(g)
 }
 
 // Twilio returns an authenticated Twilio client
 func (g *GoConnect) Twilio() *gotwilio.Twilio {
-	tool.PanicIfNil(g)
+	g.util.PanicIfNil(g)
 	return gotwilio.NewTwilioClient(g.TwilioAccount, g.TwilioToken)
 }
 
 // SendGrid returns an authenticated SendGrid client
 func (g *GoConnect) SendGrid() *sendgrid.Client {
-	tool.PanicIfNil(g)
+	g.util.PanicIfNil(g)
 	return sendgrid.NewSendClient(g.SendGridToken)
 }
 
 //Stripe returns an authenticated Stripe client
 func (g *GoConnect) Stripe(client *http.Client) *cli.API {
-	tool.PanicIfNil(g)
+	g.util.PanicIfNil(g)
 	return cli.New(g.SendGridToken, stripe.NewBackends(client))
 }
 
-//Gcloud returns an authenticated GCP instance
-func (g *GoConnect) Gcloud() *gcloud.GCP {
-	tool.PanicIfNil(g.GCP)
-	return g.GCP
+//Util returns an objectify util tool ref:github.com/autom8ter/objectify
+func (g *GoConnect) Util(client *http.Client) *objectify.Handler {
+	return g.util
 }
